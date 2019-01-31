60ca2bfec1deb813317df3c2f638b4ebb208a23e
Matt Brody <mattbrody@Matt-MBP.local>
2019-01-31 15:37:10 -0800

    -a

--- static/JS/commentbox.js
+++ static/JS/commentbox.js
@@ -1,5 +1,5 @@
-npm install commentbox.io --save
+<script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
 
-const commentBox = require('commentbox.io');
+import commentBox from 'commentbox.io';
 
 commentBox(5757464578359296-proj);
