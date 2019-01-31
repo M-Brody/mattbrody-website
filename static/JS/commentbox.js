ccda221fbc07d1be5d0bfc071cbd72b7ea77a6c7
Matt Brody <mattbrody@Matt-MBP.local>
2019-01-31 15:26:46 -0800

    -a

--- static/JS/commentbox.js
+++ static/JS/commentbox.js
@@ -1,3 +1,5 @@
 <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-import commentBox from 'commentbox.io';
+
+const commentBox = require('commentbox.io');
+
 commentBox(5757464578359296-proj);
