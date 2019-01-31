2fa9010b29d8fe1a1cb684af616ab8d4646a1680
Matt Brody <mattbrody@Matt-MBP.local>
2019-01-31 15:10:09 -0800

    -a

--- static/JS/commentboxscript.js
+++ static/JS/commentboxscript.js
@@ -1,3 +1,3 @@
 <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-<script import commentBox from 'commentbox.io';
-commentBox(5757464578359296-proj);<script>
+import commentBox from 'commentbox.io';
+commentBox(5757464578359296-proj);
